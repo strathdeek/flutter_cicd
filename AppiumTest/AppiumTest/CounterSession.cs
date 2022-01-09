6fff8eea656f5787df4892b7ab3a09a69f9008e3
Kevin Strathdee <kstrath818@gmail.com>
2022-01-09 19:04:44 +0100

    basic win32 files

---
+++ AppiumTest/AppiumTest/CounterSession.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiumTest
+{
+    public class CounterSession
+    {
+        // Note: append /wd/hub to the URL if you're directing the test at Appium
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string CounterAppId = "387D1C6E-65EB-4559-B3B9-A6BDD59EF2A5_d7c8pgvss6ysm!App";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup(TestContext context)
+        {
+            // Launch Calculator application if it is not yet launched
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the Calculator application
+                // Note: Multiple calculator windows (instances) share the same process Id
+
+                AppiumOptions options = new AppiumOptions();
+                options.AddAdditionalCapability("app", CounterAppId);
+                options.AddAdditionalCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), options);
+                Assert.IsNotNull(session);
+
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+            }
+        }
+
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
---
+++ AppiumTest/AppiumTest/CounterTest.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppiumTest
+{
+    [TestClass]
+    public class CounterTest : CounterSession
+    {
+        [TestMethod]
+        public void CounterTest1()
+        {
+            var ele0 = session.FindElementByAccessibilityId("Increment");
+            Assert.IsNotNull(ele0);
+            session.FindElementByAccessibilityId("Increment").Click();
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Create session to launch a Calculator window
+            Setup(context);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+    }
+}
